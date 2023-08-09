9d8d4011761f0a1e05195693333374dcab409a0d
root <root@mariana.izt750.ddns.net>
2023-08-09 13:26:20 -0600

    va pa riva

--- js/countdown.js
+++ js/countdown.js
@@ -1,10 +1,10 @@
 simplyCountdown('#cuenta', {
 	year: 2025, // required
-	month: 2, // required
-	day: 19, // required
-	hours: 7, // Default is 0 [0-23] integer
-	minutes: 31, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
+	month: 11, // required
+	day: 1, // required
+	hours: 13, // Default is 0 [0-23] integer
+	minutes: 22, // Default is 0 [0-59] integer
+	seconds: 19, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
 		hours: 'Hora',
